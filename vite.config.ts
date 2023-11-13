83f4eddccea628c1019b09e448bc5d2d44aef1c8
vitalii <vilavren@gmail.com>
2023-11-13 19:03:53 +0500

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      // workbox: {
+      //   maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      // },
+    }),
+  ],
+})
