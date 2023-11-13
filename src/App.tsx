57d390d6fd1aced212b91758ab2fdf950bcfdc3a
vitalii <vilavren@gmail.com>
2023-11-13 22:28:32 +0500

    added: shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
     <>
       <div className="text-red-500">Hello</div>
+      <Button>click</Button>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -14,4 +16,11 @@ export default defineConfig({
       // },
     }),
   ],
+
+  // shadcn-ui
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
