ad32144709a32efc9a37bafae602b09151b7c524
vitalii <vilavren@gmail.com>
2023-11-24 13:32:50 +0500

    added: Heading component

---
+++ src/components/common/Heading.tsx
@@ -0,0 +1,22 @@
+import { ArrowRight } from 'lucide-react'
+import { FC } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+export const Heading: FC = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Ваши идеи, документы и планы. Добро пожаловать в&nbsp;
+        <span className="underline">Note Box</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Note Box и ваша работа станет быстрее и продуктивнее.
+      </h3>
+      <Button>
+        Подключиться к Note Box
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
+    </div>
+  )
+}
--- src/pages/InitialPage.tsx
+++ src/pages/InitialPage.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 
+import { Heading } from '@/components/common/Heading'
+
 export const InitialPage: FC = () => {
   return (
     <div className="flex min-h-full flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-1 flex-col items-center justify-between gap-y-8 px-6 pb-0 text-center md:justify-between">
         Начальная страница
-        {/* <Heading /> */}
+        <Heading />
         {/* <People /> */}
         {/* <Footer /> */}
       </div>
