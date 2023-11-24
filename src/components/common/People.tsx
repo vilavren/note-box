4fc66585881c3a315a77334cac4b9073cfd76f41
vitalii <vilavren@gmail.com>
2023-11-24 13:37:59 +0500

    added: People component

---
+++ src/components/common/People.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+
+export const People: FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative h-[300px] w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]">
+          <img
+            src="/people-documents.png"
+            alt="Картинка человека с документами"
+            className="object-contain"
+          />
+        </div>
+        <div className="relative w-[400px] h-[400px] hidden md:block">
+          <img
+            src="/people-reading.png"
+            alt="Картинка человека с книгой"
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/InitialPage.tsx
+++ src/pages/InitialPage.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react'
 
 import { Heading } from '@/components/common/Heading'
+import { People } from '@/components/common/People'
 
 export const InitialPage: FC = () => {
   return (
     <div className="flex min-h-full flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-1 flex-col items-center justify-between gap-y-8 px-6 pb-0 text-center md:justify-between">
-        Начальная страница
         <Heading />
-        {/* <People /> */}
+        <People />
         {/* <Footer /> */}
       </div>
     </div>
