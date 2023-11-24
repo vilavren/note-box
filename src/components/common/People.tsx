bd99104c2bf8bb418d64369141525e9651c7dc38
vitalii <vilavren@gmail.com>
2023-11-24 15:27:33 +0500

    added: styles for pwa

--- src/components/common/People.tsx
+++ src/components/common/People.tsx
@@ -4,7 +4,7 @@ export const People: FC = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
-        <div className="relative h-[300px] w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]">
+        <div className="relative w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]">
           <img
             src="/people-documents.png"
             alt="Картинка человека с документами"
