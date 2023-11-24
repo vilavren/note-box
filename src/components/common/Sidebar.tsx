b2b30258ecb4f7256f952205e61be9c0fe576bea
vitalii <vilavren@gmail.com>
2023-11-24 16:06:01 +0500

    added: change sidebar width

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,10 +1,49 @@
 import { ChevronsLeft, MenuIcon } from 'lucide-react'
-import { FC } from 'react'
+import { ElementRef, FC, useRef } from 'react'
 
 export const Sidebar: FC = () => {
+  const isResizingRef = useRef(false)
+  const sidebarRef = useRef<ElementRef<'aside'>>(null)
+  const navbarRef = useRef<ElementRef<'div'>>(null)
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    isResizingRef.current = true
+
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isResizingRef.current) return
+
+    let newWidth = e.clientX
+
+    if (newWidth < 240) newWidth = 240
+    if (newWidth > 480) newWidth = 480
+
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`
+      navbarRef.current.style.setProperty('left', `${newWidth}px`)
+      navbarRef.current.style.setProperty('width', `calc(100% - ${newWidth}px)`)
+    }
+  }
+
+  const handleMouseUp = () => {
+    isResizingRef.current = false
+
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+  }
+
   return (
     <>
-      <aside className="group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary">
+      <aside
+        ref={sidebarRef}
+        className="group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary"
+      >
         <div
           role="button"
           className="absolute right-2 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-neutral-300 group-hover/sidebar:opacity-100 dark:hover:bg-neutral-600"
@@ -15,9 +54,15 @@ export const Sidebar: FC = () => {
         <div className="mt-4">
           <p>Документ</p>
         </div>
-        <div className="absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover/sidebar:opacity-100 " />
+        <div
+          onMouseDown={handleMouseDown}
+          className="absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover/sidebar:opacity-100 "
+        />
       </aside>
-      <div className="absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]">
+      <div
+        ref={navbarRef}
+        className="absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]"
+      >
         <nav className="w-full bg-transparent p-3">
           <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
         </nav>
